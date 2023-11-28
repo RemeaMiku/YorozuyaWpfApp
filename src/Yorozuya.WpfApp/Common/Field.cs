279fb0a67daf5efada857d2cdbdd0a9ac53cf027
aldlss <ayaldlss@gmail.com>
2023-11-28 15:34:28 +0800

    fix: 增删 field

--- src/Yorozuya.WpfApp/Common/Field.cs
+++ src/Yorozuya.WpfApp/Common/Field.cs
@@ -10,15 +10,7 @@ public class Field
 {
     public static readonly string[] Fields = {
         "政治",
-        "经济",
-        "文化",
-        "社会",
-        "科技",
-        "教育",
         "体育",
-        "军事",
-        "娱乐",
-        "健康",
         "生活",
         "学习",
         "游戏",
