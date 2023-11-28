94c2d27d1f4fbf3d1f2c47e00dd8dc4effd8aef3
aldlss <ayaldlss@gmail.com>
2023-11-28 15:30:52 +0800

    feat: 完善主页和用户界面

---
+++ src/Yorozuya.WpfApp/Common/Converters/IEnumerableToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Yorozuya.WpfApp.Common.Converters;
+
+/// <summary>
+/// 可列举对象转换为可见性
+/// 如果对象为长度为 0，则返回 Collapsed，否则返回 Visible
+/// </summary>
+public class IEnumerableToVisibilityConverter: IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var enumerableValue = (IEnumerable<object>)value;
+        return enumerableValue is null || enumerableValue.Any() ? System.Windows.Visibility.Collapsed : System.Windows.Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/Yorozuya.WpfApp/Views/Pages/HomePage.xaml.cs
+++ src/Yorozuya.WpfApp/Views/Pages/HomePage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using CommunityToolkit.Mvvm.Messaging;
+using Yorozuya.WpfApp.Extensions;
 using Yorozuya.WpfApp.ViewModels.Pages;
 
 namespace Yorozuya.WpfApp.Views.Pages;
@@ -8,11 +13,42 @@ namespace Yorozuya.WpfApp.Views.Pages;
 /// </summary>
 public partial class HomePage : Page
 {
-    public HomePage(HomePageViewModel viewModel)
+    public HomePage(HomePageViewModel viewModel, IMessenger messenger)
     {
         InitializeComponent();
         DataContext = this;
         ViewModel = viewModel;
+
+        messenger.Register<HomePage, string, string>(this, nameof(HomePage),  async (homePage, message) =>
+        {
+            var duration = TimeSpan.FromMilliseconds(500);
+            var marginTop = new Thickness(0, 500, 0, 0);
+            var marginLeft = new Thickness(800, 0, 0, 0);
+            var easing = new QuadraticEase { EasingMode = EasingMode.EaseInOut };
+            switch (message)
+            {
+                case HomePageViewModel.UiStatus.StartSearch:
+                    if(homePage.SearchResultGrid.Visibility == Visibility.Visible) break;
+                    homePage.SearchResultGrid.Visibility = Visibility.Visible;
+                    await homePage.SearchResultGrid.SlideAndFadeInAsync(duration, marginTop, easing);
+                    break;
+                case HomePageViewModel.UiStatus.EndSearch:
+                    if (homePage.SearchResultGrid.Visibility == Visibility.Collapsed) break;
+                    await homePage.SearchResultGrid.SlideAndFadeOutAsync(duration, marginTop, easing);
+                    homePage.SearchResultGrid.Visibility = Visibility.Collapsed;
+                    break;
+                case HomePageViewModel.UiStatus.StartPost:
+                    if (homePage.AddNewPostGrid.Visibility == Visibility.Visible) break;
+                    homePage.AddNewPostGrid.Visibility = Visibility.Visible;
+                    await homePage.AddNewPostGrid.SlideAndFadeInAsync(duration, marginLeft, easing);
+                    break;
+                case HomePageViewModel.UiStatus.EndPost:
+                    if (homePage.AddNewPostGrid.Visibility == Visibility.Collapsed) break;
+                    await homePage.AddNewPostGrid.SlideAndFadeOutAsync(duration, marginLeft, easing);
+                    homePage.AddNewPostGrid.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        });
     }
 
     public HomePageViewModel ViewModel { get; }
